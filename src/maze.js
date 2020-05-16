d65cc8b35c5e9e060cd56f65d524c0289b815f38
RCDMK <rcdmk@hotmail.com>
2020-05-17 01:39:48 +0200

    Change map distance calculation to use A* path length

--- src/maze.js
+++ src/maze.js
@@ -21,6 +21,9 @@ class Maze {
             x: 3,
             y: 1
         };
+
+        // longest path possible
+        this.maxPathLength = (this.cols * this.rows) * 0.65;
     }
 
     get rows() {
@@ -32,11 +35,117 @@ class Maze {
     }
 
     get maxDistance() {
-        return this.cols + this.rows;
+        return this.maxPathLength;
     }
 
     calculateDistance(startX, startY, targetX, targetY) {
-       return abs(targetX - startX) + abs(targetY - startY);
+       return this.computeAStarPath(startX, startY, targetX, targetY).length;
+    }
+
+    #buildVisitedKey(x, y) {
+        return x + "," + y;
+    }
+
+    computeAStarPath(startX, startY, targetX, targetY) {
+        let queue = [{
+            x: targetX,
+            y: targetY,
+            cost: 0,
+        }];
+
+        let curr;
+        let visited = new Map();
+
+        while(queue.length > 0) {
+            curr = queue.shift();
+            visited.set(this.#buildVisitedKey(curr.x, curr.y), curr);
+            if (curr.x == startX && curr.y == startY) break;
+
+            let neighbours = [];
+
+            if (curr.y - 1 >= 0 && this.map[curr.y - 1][curr.x] === 0) {
+                if (!visited.has(this.#buildVisitedKey(curr.x, curr.y - 1))) {
+                    neighbours.push({ x: curr.x, y: curr.y - 1, cost: curr.cost + 1 });
+                }
+            }
+
+            if (curr.x + 1 < this.cols && this.map[curr.y][curr.x + 1] === 0) {
+                if (!visited.has(this.#buildVisitedKey(curr.x + 1, curr.y))) {
+                    neighbours.push({ x: curr.x + 1, y: curr.y, cost: curr.cost + 1 });
+                }
+            }
+
+            if (curr.y + 1 < this.rows && this.map[curr.y + 1][curr.x] === 0) {
+                if (!visited.has(this.#buildVisitedKey(curr.x, curr.y + 1))) {
+                    neighbours.push({ x: curr.x, y: curr.y + 1, cost: curr.cost + 1 });
+                }
+            }
+
+            if (curr.x - 1 >= 0 && this.map[curr.y][curr.x - 1] === 0) {
+                if (!visited.has(this.#buildVisitedKey(curr.x - 1, curr.y))) {
+                    neighbours.push({ x: curr.x - 1, y: curr.y, cost: curr.cost + 1 });
+                }
+            }
+
+            for(let neighbour of neighbours) {
+                let found = queue.filter(cell => cell.x == neighbour.x && cell.y == neighbour.y);
+                if (found.length > 0 && found[0].cost > neighbour.cost) {
+                    found[0].cost = neighbour.cost;
+                } else if (!visited.has(this.#buildVisitedKey(neighbour.x, neighbour.y))) {
+                    queue.push(neighbour);
+                }
+            }
+        }
+
+        // no path possible
+        if (queue.length === 0 && (curr.x !== startX || curr.y !== startY)) return [];
+        queue.length = 0;
+
+        let path = [curr];
+
+        while (curr.x != targetX || curr.y != targetY) {
+            // top
+            if (curr.y - 1 >= 0 && this.map[curr.y - 1][curr.x] === 0) {
+                let found = visited.get(this.#buildVisitedKey(curr.x, curr.y - 1));
+                if (found && found.cost < curr.cost) {
+                    curr = found;
+                    path.push(curr);
+                    continue;
+                }
+            }
+
+            // right
+            if (curr.x + 1 < this.cols && this.map[curr.y][curr.x + 1] === 0) {
+                let found = visited.get(this.#buildVisitedKey(curr.x + 1, curr.y));
+                if (found && found.cost < curr.cost) {
+                    curr = found;
+                    path.push(curr);
+                    continue;
+                }
+            }
+
+            // bottom
+            if (curr.y + 1 < this.rows && this.map[curr.y + 1][curr.x] === 0) {
+                let found = visited.get(this.#buildVisitedKey(curr.x, curr.y + 1));
+                if (found && found.cost < curr.cost) {
+                    curr = found;
+                    path.push(curr);
+                    continue;
+                }
+            }
+
+            // left
+            if (curr.x - 1 >= 0 && this.map[curr.y][curr.x - 1] === 0) {
+                let found = visited.get(this.#buildVisitedKey(curr.x - 1, curr.y));
+                if (found && found.cost < curr.cost) {
+                    curr = found;
+                    path.push(curr);
+                    continue;
+                }
+            }
+        }
+
+        return path;
     }
 
     show() {
